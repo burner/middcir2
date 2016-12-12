936dae63d112037a7ec5e1cc0b4addf30fcdbe5d
Robert burner Schadek <rburners@gmail.com>
2016-12-12 17:18:01 +0100

    some more on stats

--- source/stats.d
+++ source/stats.d
@@ -1,5 +1,12 @@
 module stats;
 
+import std.experimental.logger;
+
+import utils;
+import mapping;
+import graph;
+import graphgen;
+import protocols;
 import protocols.mcs;
 import protocols.grid;
 import protocols.lattice;
@@ -19,11 +26,35 @@ void runMappings(string graphsFilename) {
 	const(long) size = graphs.front.length;
 	Lattice[] lattices;
 	Grid[] grids;
-	buildGraphBase(lattices, grids, size);
+	buildGraphBased(lattices, grids, size);
+	auto mcs = MCS(cast(int)size);
+
+	Result[] latticeResults;	
+	Result[] gridResults;	
+	Result mcsResult;	
+
+	logf("%s", latticeResults.length);
+	runNormal(lattices, latticeResults, grids, gridResults, mcs, mcsResult);
+	logf("%s", latticeResults.length);
 
 	foreach(g; graphs) {
 		runMapping(g);
 	}
+}
+
+void runNormal(ref Lattice[] lattices, ref Result[] latticeResults, 
+		ref Grid[] grids, ref Result[] gridResults,
+		ref MCS mcs, ref Result mcsResult) 
+{
+	foreach(ref it; lattices) {
+		latticeResults ~= it.calcAC();
+	}
+
+	foreach(ref it; grids) {
+		gridResults ~= it.calcAC();
+	}
+
+	mcsResult = mcs.calcAC();
 }
 
 void buildGraphBased(ref Lattice[] lattices, ref Grid[] grids,
@@ -37,9 +68,17 @@ void buildGraphBased(ref Lattice[] lattices, ref Grid[] grids,
 	}
 }
 
-void runMapping(int Size)(auto ref Graph!Size g) {
+void runMapping(int Size)(auto ref Graph!Size g, Lattice[] lattices,
+	   	Grid[] grids, MCS mcs)
+{
 	auto row = [ROW(0.01), ROW(0.1), ROW(0.25), ROW(0.5), ROW(0.75),
-			ROW(0.9), ROW(0.99)];
+			ROW(0.9), ROW(0.99)
+		];
 	auto rowc = [ROWC(0.01), ROWC(0.1), ROWC(0.25), ROWC(0.5), ROWC(0.75),
-			ROWC(0.9), ROWC(0.99)];
+			ROWC(0.9), ROWC(0.99)
+		];
+}
+
+void runMapping(int Size)(auto ref Graph!Size g, Lattice[] lattices) {
+
 }
