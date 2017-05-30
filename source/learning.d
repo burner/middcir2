b0af03b04628d4e9020567692e20983b3d0297be
Robert burner Schadek <rburners@gmail.com>
2017-05-30 17:25:30 +0200

    learning is getting more work

--- source/learning.d
+++ source/learning.d
@@ -9,29 +9,107 @@ import std.meta : AliasSeq;
 import fixedsizearray;
 
 import statsanalysis;
+import permutation;
 import bitsetmodule;
 
-interface IStat(int Size) {
-	@property string XLabel() const;
-	double select(const(GraphStats!Size) g);
-	double select(const(GraphWithProperties!Size) g);
+string shortName(string l) {
+	import std.uni : isNumber;
+	string ret;
+	foreach(char c; l) {
+		if(c == ' ') {
+			ret ~= '_';
+		} else if(c != 'a' && c != 'i' && c != 'o' && c != 'u' && c != 'e' 
+				&& c != '!' && !isNumber(c) && c != ',')
+		{
+			ret ~= c;
+		}
+	}
+	return ret;
 }
 
-class CStat(int Size, Stat) {
-	@property string XLabel() const {
+abstract class IStat(int Size) {
+	string name;
+	abstract @property string XLabel() const;
+	abstract double select(const(GraphStats!Size) g) const;
+	abstract double select(const(GraphWithProperties!Size) g) const;
+}
+
+class CStat(alias Stat, int Size) : IStat!Size {
+	this() {
+		this.name = shortName(Stat!Size.XLabel);
+	}
+	override @property string XLabel() const {
 		return Stat!Size.XLabel;
 	}
 
-	double select(const(GraphStats!Size) g) {
+	override double select(const(GraphStats!Size) g) const {
 		return Stat!Size.select(g);
 	}
-	double select(const(GraphWithProperties!Size) g) {
+	override double select(const(GraphWithProperties!Size) g) const {
 		return Stat!Size.select(g);
-	}	
+	}
+}
+
+auto cstatsArray = [
+	new CStat!(DiameterAverage,32), new CStat!(DiameterMedian,32),
+	new CStat!(DiameterMax,32), new CStat!(DiameterMode,32),
+	new CStat!(Connectivity,32), new CStat!(DegreeAverage,32), 
+	new CStat!(DegreeMedian,32), new CStat!(DegreeMin,32), 
+	new CStat!(DegreeMax,32), new CStat!(BetweenneesAverage,32), 
+	new CStat!(BetweenneesMedian,32), new CStat!(BetweenneesMin,32), 
+	new CStat!(BetweenneesMax,32)
+];
+
+class MMCStat(int Size) {
+	FixedSizeArray!(IStat!Size) cstats;
+
+	bool less(const(GraphStats!Size) a, 
+			const(GraphStats!Size) b) const 
+	{
+		import std.math : approxEqual;
+
+		foreach(it; this.cstats) {
+			if(approxEqual(it.select(a), it.select(b), 0.000_001)) {
+				continue;
+			} else if(it.select(a) < it.select(b)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	void insertIStat(IStat!Size ne) {
+		this.cstats.insertBack(ne);
+	}
+
+	void clear() {
+		this.cstats.removeAll();
+	}
 }
 
 unittest {
-	auto n = new CStat!(16,Connectivity!16)();
+	IStat!32 n = new CStat!(Connectivity, 32)();
+	n = new CStat!(DiameterAverage,32)();
+
+	int count;
+	for(int i = 0; i < cstatsArray.length; ++i) {
+		auto permu = Permutations(cast(int)cstatsArray.length, i, cast(int)cstatsArray.length);
+		auto mm = new MMCStat!32();
+		foreach(perm; permu) {
+			for(int j = 0; j < cstatsArray.length; ++j) {
+				if(perm.test(j)) {
+					mm.insertIStat(cstatsArray[j]);
+					write(cstatsArray[j].name, " ");
+				}
+			}
+			mm.clear();
+			writeln();
+			++count;
+		}
+	}
+	writeln(count);
 }
 
 // how good can "mm" can be used to predict the costs or availability
